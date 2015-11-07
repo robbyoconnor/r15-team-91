7b303bf32ffa738a2ec9253c69eeb6dc80b5bc1f
Leonardo Saraiva <vyper@maneh.org>
2015-11-07 10:48:26 -0200

    Added repository model

---
+++ app/models/repository.rb
@@ -0,0 +1,31 @@
+class Repository
+  #
+  # attributes
+  #
+  ATTRIBUTES = %i(name description fork created_at pushed_at homepage
+                  language stargazers_count forks_count open_issues_count
+                  network_count subscribers_count watchers_count)
+
+  attr_accessor :repository
+
+  #
+  # methods
+  #
+  def initialize(repository:)
+    @repository = repository
+  end
+
+  def self.find_by(owner:, name:)
+    new(repository: Octokit.repository(owner: owner, name: name))
+  rescue Octokit::NotFound => e
+    nil
+  end
+
+  private
+
+  def method_missing(method_sym, *_arguments, &_block)
+    return repository[method_sym] if ATTRIBUTES.include? method_sym
+
+    super
+  end
+end
