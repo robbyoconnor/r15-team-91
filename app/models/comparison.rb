e263bded583521a90019cda5e3abfcb04cad7746
Leonardo Saraiva <vyper@maneh.org>
2015-11-07 18:49:29 -0200

    Added validation to accept only comparison with two repositories

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -8,4 +8,13 @@ class Comparison < ActiveRecord::Base
   # validations
   #
   validates :code, presence: true, uniqueness: true
+  validate :need_twice_repositories
+
+  private
+
+  def need_twice_repositories
+    if repositories.size != 2
+      errors.add(:base, 'It is necessary two repositories for make a comparison') # TODO: Locale?
+    end
+  end
 end
